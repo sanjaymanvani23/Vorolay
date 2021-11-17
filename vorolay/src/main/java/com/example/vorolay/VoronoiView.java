2f91bdcc4c872233780b41216cba7b4112f963d6
Sanjay <sanjaymanvani23@gmail.com>
2021-11-17 14:19:07 +0530

    create library

---
+++ vorolay/src/main/java/com/example/vorolay/VoronoiView.java
@@ -0,0 +1,55 @@
+package com.example.vorolay;
+
+
+
+import java.util.List;
+
+public class VoronoiView {
+
+    // ************* Private members ******************
+    private double borderMinX;
+    private double borderMaxX;
+    private double borderMinY;
+    private double borderMaxY;
+
+    private int siteidx;
+
+    private double xmin;
+    private double ymin;
+
+    private double deltax;
+    private double deltay;
+
+    private int nvertices;
+    private int nedges;
+    private int nsites;
+
+    //private Site[] sites;
+    //private Site bottomsite;
+
+    private int sqrt_nsites;
+
+    private double minDistanceBetweenSites;
+
+    private int PQcount;
+    private int PQmin;
+    private int PQhashsize;
+
+    //private Halfedge PQhash[];
+
+    private final static int LE = 0;
+    private final static int RE = 1;
+
+    private int ELhashsize;
+   // private Halfedge ELhash[];
+    //private Halfedge ELleftend, ELrightend;
+    //private List<GraphEdge> allEdges;
+
+
+
+    /*********************************************************
+     * Public methods
+     ********************************************************/
+
+
+}
